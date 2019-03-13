9566cb180523d0774f27b940353f400dc0727ddd
Kyle Roach <kroach.work@gmail.com>
2019-03-12 23:19:46 -0400

    feat(website): Update site title and author

--- packages/website/gatsby-config.js
+++ packages/website/gatsby-config.js
@@ -4,9 +4,9 @@ require('dotenv').config({
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Good News`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `East Caribbean Conference of SDA`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
