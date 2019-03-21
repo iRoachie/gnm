4a80261ea6c723ff897b6c7a87a154cbe5e925e2
Kyle Roach <kroach.work@gmail.com>
2019-03-21 00:29:13 -0400

    feat(mobile): Add contacts list functionality

--- packages/mobile/src/pages/Contacts.tsx
+++ packages/mobile/src/pages/Contacts.tsx
@@ -1,13 +1,44 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import { Appbar, DefaultTheme, FAB } from 'react-native-paper';
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet, Platform, FlatList } from 'react-native';
+import {
+  Appbar,
+  DefaultTheme,
+  FAB,
+  List,
+  ActivityIndicator,
+} from 'react-native-paper';
 import { NavigationScreenProps } from 'react-navigation';
+
+import { SearchBar } from 'react-native-elements';
 
 const Contacts: React.StatelessComponent<NavigationScreenProps> = ({
   navigation,
 }) => {
+  const [search, setSearch] = useState('');
+  const [contacts, setContacts] = useState<any[]>([]);
+  const [fetching, setFetching] = useState(true);
+
+  useEffect(() => {
+    const getContacts = async () => {
+      const response = await fetch('https://randomuser.me/api/?results=20');
+      const result = await response.json();
+      setContacts(result.results);
+      setFetching(false);
+    };
+
+    getContacts();
+  }, []);
+
   const goToNewContact = () => {
     navigation.navigate('NewContact');
+  };
+
+  const toggleFilter = () => {
+    //
+  };
+
+  const viewContact = (contact: any) => {
+    //
   };
 
   return (
@@ -17,7 +48,35 @@ const Contacts: React.StatelessComponent<NavigationScreenProps> = ({
           title="Contacts"
           titleStyle={{ fontFamily: DefaultTheme.fonts.medium, fontSize: 17 }}
         />
+
+        <Appbar.Action icon="filter-list" onPress={toggleFilter} />
       </Appbar.Header>
+
+      <SearchBar
+        platform={Platform.OS === 'ios' ? 'ios' : 'android'}
+        placeholder="Search Contacts"
+        inputStyle={{
+          fontSize: Platform.OS === 'ios' ? 17 : 15,
+        }}
+        value={search}
+        onChangeText={setSearch}
+      />
+
+      {fetching ? (
+        <ActivityIndicator style={{ marginTop: 16 }} />
+      ) : (
+        <FlatList
+          data={contacts}
+          keyExtractor={item => item.login.uuid}
+          renderItem={({ item }) => (
+            <List.Item
+              title={`${item.name.first} ${item.name.last}`}
+              description={item.location.street}
+              onPress={() => viewContact(item)}
+            />
+          )}
+        />
+      )}
 
       <FAB style={styles.fab} icon="add" onPress={goToNewContact} />
     </View>
