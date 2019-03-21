5d86ab8f034ac77f1c52f1843f8f5c6ffe10cda8
Kyle Roach <kroach.work@gmail.com>
2019-03-21 02:21:51 -0400

    feat(mobile): Wrap dashboard in ScrollView

--- packages/mobile/src/pages/Dashboard/Dashboard.tsx
+++ packages/mobile/src/pages/Dashboard/Dashboard.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
-import { View, StyleSheet, Text, TouchableHighlight } from 'react-native';
+import {
+  View,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  ScrollView,
+} from 'react-native';
 import { Appbar, DefaultTheme } from 'react-native-paper';
 import { Divider } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/MaterialIcons';
@@ -25,72 +31,76 @@ const Dashboard: React.StatelessComponent<NavigationScreenProps> = ({
         />
       </Appbar.Header>
 
-      <View style={{ padding: 16 }}>
-        <View style={styles.banner}>
-          <View
-            style={{
-              alignItems: 'flex-start',
-              flexDirection: 'row',
-              padding: 10,
-              paddingTop: 15,
-            }}
-          >
-            <Icon
-              name="assignment-ind"
-              color="rgba(255,255,255,0.7)"
-              size={40}
-            />
+      <ScrollView>
+        <View style={{ padding: 16 }}>
+          <View style={styles.banner}>
+            <View
+              style={{
+                alignItems: 'flex-start',
+                flexDirection: 'row',
+                padding: 10,
+                paddingTop: 15,
+              }}
+            >
+              <Icon
+                name="assignment-ind"
+                color="rgba(255,255,255,0.7)"
+                size={40}
+              />
 
-            <View style={{ marginLeft: 16 }}>
-              <Text style={styles.totalContactsLabel}>{thousands('1350')}</Text>
-              <Text style={styles.totalContactsCaption}>Total Persons</Text>
+              <View style={{ marginLeft: 16 }}>
+                <Text style={styles.totalContactsLabel}>
+                  {thousands('1350')}
+                </Text>
+                <Text style={styles.totalContactsCaption}>Total Persons</Text>
+              </View>
             </View>
+
+            <TouchableHighlight
+              underlayColor="rgba(255,255,255,.7)"
+              onPress={addContact}
+              style={{
+                width: 80,
+                backgroundColor: '#fff',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+            >
+              <Icon color={Theme.primary} size={30} name="add" />
+            </TouchableHighlight>
           </View>
 
-          <TouchableHighlight
-            underlayColor="rgba(255,255,255,.7)"
-            onPress={addContact}
-            style={{
-              width: 80,
-              backgroundColor: '#fff',
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}
-          >
-            <Icon color={Theme.primary} size={30} name="add" />
-          </TouchableHighlight>
-        </View>
+          <Divider style={{ marginVertical: 30 }} />
 
-        <Divider style={{ marginVertical: 30 }} />
+          <View style={{ flexDirection: 'row' }}>
+            <DashboardSquare
+              caption="Contacts"
+              value={14}
+              backgroundColor="#3F51B5"
+            />
+            <View style={{ width: 10 }} />
+            <DashboardSquare
+              caption="Interests"
+              value={24}
+              backgroundColor="#E64A19"
+            />
+          </View>
 
-        <View style={{ flexDirection: 'row' }}>
-          <DashboardSquare
-            caption="Contacts"
-            value={14}
-            backgroundColor="#3F51B5"
-          />
-          <View style={{ width: 10 }} />
-          <DashboardSquare
-            caption="Interests"
-            value={24}
-            backgroundColor="#E64A19"
-          />
+          <View style={{ flexDirection: 'row' }}>
+            <DashboardSquare
+              caption="Prospects"
+              value={13}
+              backgroundColor="#4CAF50"
+            />
+            <View style={{ width: 10 }} />
+            <DashboardSquare
+              caption="Members"
+              value={0}
+              backgroundColor="#FFA000"
+            />
+          </View>
         </View>
-
-        <View style={{ flexDirection: 'row' }}>
-          <DashboardSquare
-            caption="Prospects"
-            value={13}
-            backgroundColor="#4CAF50"
-          />
-          <View style={{ width: 10 }} />
-          <DashboardSquare
-            caption="Members"
-            value={0}
-            backgroundColor="#FFA000"
-          />
-        </View>
-      </View>
+      </ScrollView>
     </View>
   );
 };
