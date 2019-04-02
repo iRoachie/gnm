73e33c442f13dd8dab32937d4878bf67415ca299
Kyle Roach <kroach.work@gmail.com>
2019-04-02 07:52:41 -0400

    feat(server): Send back count in persons query

--- packages/server/src/resolvers/Query/persons.js
+++ packages/server/src/resolvers/Query/persons.js
@@ -14,7 +14,11 @@ const persons = async (parent, args, context) => {
   const { permissions, user } = await validateJWT(context.token);
 
   if (permissions.includes('Person:ListAll')) {
-    return context.prisma.persons(args);
+    const persons = await context.prisma.persons(args);
+    return {
+      count: persons.length,
+      data: persons,
+    };
   }
 
   if (permissions.includes('Person:ListArea')) {
@@ -24,23 +28,33 @@ const persons = async (parent, args, context) => {
       })
       .contactSites()).map(a => a.id);
 
-    return context.prisma.persons({
+    const persons = await context.prisma.persons({
       ...args,
       where: {
         ...args.where,
         contactSite: { id_in: sites },
       },
     });
+
+    return {
+      count: persons.length,
+      data: persons,
+    };
   }
 
   if (permissions.includes('Person:ListAssigned')) {
-    return context.prisma.persons({
+    const persons = await context.prisma.persons({
       ...args,
       where: {
         ...args.where,
         assignee: { id: user.id },
       },
     });
+
+    return {
+      count: persons.length,
+      data: persons,
+    };
   }
 
   throw new InsufficientPermissionsError();
