bd0de8ba382f4c9f5fc6b903b0602d232d915bec
Kyle Roach <kroach.work@gmail.com>
2019-04-10 22:57:41 -0400

    feat(mobile): Add send feedback

--- mobile/src/pages/Settings.tsx
+++ mobile/src/pages/Settings.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from 'react';
-import { View } from 'react-native';
+import { View, Linking, Platform } from 'react-native';
 import { Appbar, List } from 'react-native-paper';
 import { NavigationScreenProps } from 'react-navigation';
 
@@ -22,6 +22,16 @@ const Settings: React.StatelessComponent<NavigationScreenProps> = ({
     }, 2000);
   };
 
+  const sendFeedback = async () => {
+    try {
+      await Linking.openURL(
+        `mailto:hello.gnm2019@gmail.com?subject=GNM 2019 App Feedback&body=Hello Team GNM!\n\n\n\n\n------------\n\nPlatform: ${
+          Platform.OS
+        }\nVersion: ${Platform.Version}`
+      );
+    } catch {}
+  };
+
   return (
     <View style={{ flex: 1 }}>
       <Loading visible={loading} />
@@ -34,6 +44,15 @@ const Settings: React.StatelessComponent<NavigationScreenProps> = ({
         title="Options"
         theme={{ fonts: { medium: Theme.fonts.medium } }}
       >
+        <List.Item
+          title="Send feedback"
+          description="Contact admin team for feedback/issues"
+          titleStyle={{ fontFamily: Theme.fonts.medium }}
+          descriptionStyle={{ fontFamily: Theme.fonts.regular }}
+          left={props => <List.Icon {...props} icon="help" />}
+          onPress={sendFeedback}
+        />
+
         <List.Item
           title="Sign out"
           description="Sign out of the app"
