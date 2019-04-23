cfea95800d43465579e99f640e4a2ed378746c6a
Kyle Roach <kroach.work@gmail.com>
2019-04-23 00:28:09 -0400

    fix(server): Return team info in Person query

--- server/src/resolvers/Person.js
+++ server/src/resolvers/Person.js
@@ -1,9 +1,11 @@
 const contactSite = require('../getters/contactSite');
 const status = require('../getters/status');
+const team = require('../getters/team');
 
 const Person = {
   contactSite,
   status,
+  team,
 };
 
 module.exports = Person;
