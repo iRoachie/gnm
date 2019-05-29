a652269cd08074dbc61ab2ee9c4f6dd8f0773def
Kyle Roach <kroach.work@gmail.com>
2019-05-29 10:16:15 -0400

    fix(website): Bug where watch video wasn't showing on mobile
    the check on the watch page for window (gatsy) result in NaNpx for the vieo player height on mobile. Instead of using a hook and window listener, opted to use css calc to calculate the video height

--- website/src/components/Watch/Player.js
+++ website/src/components/Watch/Player.js
@@ -1,59 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
-function useWindowWidth() {
-  let initialWidth;
+const Player = ({ youtubeId }) => (
+  <>
+    <div className="video-container relative mx-auto">
+      <iframe
+        className="absolute top-0 left-0 w-full h-full"
+        width="560"
+        height="315"
+        name="Video Player"
+        src={`https://www.youtube.com/embed/${youtubeId}?showinfo=0&rel=0`}
+        frameBorder="0"
+        allowFullScreen
+      />
+    </div>
 
-  if (typeof window !== 'undefined') {
-    initialWidth = window.innerWidth;
-  }
+    <style jsx>{`
+      .video-container {
+        height: calc(100vw / 16 * 9);
+        max-height: calc(1108px / 16 * 9);
+        max-width: calc(1140px - 2rem);
+      }
+    `}</style>
+  </>
+);
 
-  const [width, setWidth] = useState(initialWidth);
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const handleResize = () => setWidth(window.innerWidth);
-      window.addEventListener('resize', handleResize);
-      return () => {
-        window.removeEventListener('resize', handleResize);
-      };
-    }
-  });
-
-  return width;
-}
-
-function calculateVideoWidth(width) {
-  return (width / 16) * 9;
-}
-
-export default function Player({ youtubeId }) {
-  const width = useWindowWidth();
-  const [videoHeight, setVideoHeight] = useState(calculateVideoWidth(width));
-
-  useEffect(() => {
-    setVideoHeight(calculateVideoWidth(width > 1108 ? 1108 : width));
-  }, [width]);
-
-  return (
-    <>
-      <div className="video-container relative mx-auto">
-        <iframe
-          className="absolute top-0 left-0 w-full h-full"
-          width="560"
-          height="315"
-          name="Video Player"
-          src={`https://www.youtube.com/embed/${youtubeId}?showinfo=0&rel=0`}
-          frameBorder="0"
-          allowFullScreen
-        />
-      </div>
-
-      <style jsx>{`
-        .video-container {
-          min-height: ${videoHeight}px;
-          max-width: calc(1140px - 2rem);
-        }
-      `}</style>
-    </>
-  );
-}
+export default Player;
