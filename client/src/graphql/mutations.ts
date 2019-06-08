5414d0f7bc295027affc5c9760f236e4ae5b769c
Kyle Roach <kroach.work@gmail.com>
2019-06-08 13:50:51 -0400

    feat: Remove client

--- client/src/graphql/mutations.ts
+++
@@ -1,7 +0,0 @@
-import gql from 'graphql-tag';
-
-export const registerAttendanceMutation = gql`
-  mutation {
-
-  }
-`;
--- client/src/pages/index.ts
+++
@@ -1,5 +0,0 @@
-import Login from './Login';
-import Contacts from './Contacts';
-import ViewContact from './ViewContact';
-
-export { Login, Contacts, ViewContact };
